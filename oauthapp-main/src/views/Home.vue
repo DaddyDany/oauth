5e963ea9d3ad0678c50d717c66ad982c287cf66b
DaddyDany <20213tn101@utez.edu.mx>
2024-04-19 10:34:30 -0600

    first commit

---
+++ oauthapp-main/src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="home"
+    style="
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      top: -50px;
+    "
+  >
+    <div>
+      <h1>Inicio de sesión con Oauth</h1>
+      <div v-if="!$auth.loading">
+        <b-button
+          variant="outline-primary"
+          v-if="!$auth.isAuthenticated"
+          @click="login"
+          >Iniciar Sesión</b-button
+        >
+        <b-button
+          variant="outline-danger"
+          v-if="$auth.isAuthenticated"
+          @click="logout"
+          >Cerrar Sesión</b-button
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        logoutParams: {
+          returnTo: window.location.origin,
+        },
+      });
+    },
+  },
+};
+</script>
